bbb9643168ddbb066fce1da16e1f2b7398e4a9ac
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-06-26 00:05:27 +0530

    chore: cleaning up with readme

--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-	adapter: adapter({
-		pages: 'build',
-		assets: 'build',
-		fallback: undefined,
-		precompress: false,
-		strict: true
-	}),
-},
+	adapter: adapter(),
+	},
 	preprocess: vitePreprocess(),
 };
 
